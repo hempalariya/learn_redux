7a85b6a1e7cd7703d4de9fb587e4d53a0eab3371
Hem Palariya <palariya8513@gmail.com>
2026-01-17 07:43:20 +0530

    initial commit just with redux store and action creators

---
+++ src/store.js
@@ -0,0 +1,65 @@
+import { createStore } from "redux"
+
+const initialState= {
+    balance: 0,
+    loan: 0,
+    loanPurpose: ""
+}
+
+
+function reducer (state = initialState, action){
+    switch(action.type){
+        case "account/deposit":
+            return {...state, balance: state.balance + action.payload}
+        case "account/withdraw":
+            return {...state, balance: state.balance - action.payload}
+        case "account/requestLoan":
+            return {...state, loan: action.payload.amount, loanPurpose: action.payload.purpose, balance: state.balance + action.payload.amount}
+        case "account/payLoan":
+            return {...state, loan: 0, loanPurpose: '', balance: state.balance - state.loan}
+        default:
+            return state
+    }
+}
+
+
+const store = createStore(reducer)
+
+// store.dispatch({type: "account/deposit", payload: 500})
+// console.log(store.getState())
+// store.dispatch({type: "account/withdraw", payload: 200})
+// console.log(store.getState())
+// store.dispatch({type: "account/requestLoan", payload:{
+//     amount: 500, purpose: "to buy a laptop"
+// }})
+// console.log(store.getState())
+// store.dispatch({type: "account/payLoan"})
+// console.log(store.getState())
+
+
+//action creators
+
+function deposit(amount){
+    return {type: "account/deposit", payload: amount}
+}
+
+function withdraw(amount){
+    return {type: "account/withdraw", payload: amount}
+}
+
+function requestLoan(amount, purpose){
+    return {type: "account/requestLoan", payload: {amount: amount, purpose: purpose}}
+}
+
+function payLoan(){
+    return {type: "account/payLoan"}
+}
+
+store.dispatch(deposit(500))
+console.log(store.getState())
+store.dispatch(withdraw(300))
+console.log(store.getState())
+store.dispatch(requestLoan(800, 'to buy a laptop'))
+console.log(store.getState())
+store.dispatch(payLoan())
+console.log(store.getState())
